7885e5608992078b8f85eba28d75c60a4b1a65b0
K1426 <ksohail.bese24seecs#seecs.edu.pk>
2025-09-18 23:58:38 -0700

    Add hello.cpp starter program

---
+++ hello.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Welcome to Data Structures and Algorithms! Also to GitHub, which is the point of this lab." << std::endl;
+    //random sentence
+    return 0;
+}
