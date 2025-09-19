393215fbcf71129cf0fc7cd0c0dfd6ea9eb436a3
K1426 <ksohail.bese24seecs@seecs.edu.pk>
2025-09-19 04:12:42 -0700

    Implementaion of strings in heap

---
+++ string_reverse.cpp
@@ -0,0 +1,24 @@
+//string_reverse
+#include <iostream>
+using namespace std;
+
+void reverse_string(string* ptr)
+{
+    string str = "";
+    //a string var to store the reversed version
+    int size = (*ptr).length();
+    for (int i = 1; i <= size; i++)
+        str+=(*ptr)[size - i];
+    *ptr = str;
+    //assign the reversed sring value to the value at address associated with the pointer
+}
+
+int main()
+{
+    string *s = new string("");
+    cout << "Please enter a word, phrase or sentence:\n";
+    getline(cin, *s);
+    reverse_string(s);
+    cout << "The reverse of the string you entered is:\n" << *s;
+    return 0;
+}
